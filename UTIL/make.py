5a2606f4a15199899821ededbb85d005e2c82fdd
Mike Mihailov <photo.mickey@gmail.com>
2020-03-04 12:00:54 +0300

    Compilator ready

--- UTIL/make.py
+++ UTIL/make.py
@@ -27,7 +27,8 @@ def minifyCss(css):
     return cssmin(css, keep_bang_comments=True)
 #*******************************************************************************
 def  minifyJs(js):
-    return jsmin(js, keep_bang_comments=False)
+    out = jsmin(js, keep_bang_comments=False)
+    return out
 #*******************************************************************************
 def minifyHtml(html):
     index = 1
@@ -35,22 +36,49 @@ def minifyHtml(html):
     fl = 0
     while (index > 0):
         index = out.find(">",index+1)
-        comm = 0
         if (index > 0):
             subindex = out.find("<",index)
-            if (out[subindex+1] == '!'):
-                comm = 0
-                comIndex = out.find(">",subindex)
-            line = out[index+1:subindex]
+            line = out[(index + 1):subindex]
             flag = 1
             for i in range(0,len(line)):
                 if ((line[i] != " ") and (line[i] != "\n") and (line[i] != "\t") and (line[i] != "\r") and (ord(line[i]) != 0x0A)):
                     flag = 0
             if (flag == 1):
-                if (comm == 1):
-                    subindex = comIndex+1
-                out = out[:index+1]+out[subindex:]
+                out = out[:(index + 1)] + out[subindex:]
+    index = 1
+    subindex = 1
+    while (index > 0):
+        index = out.find("<!--",index+1)
+        if(index > 0):
+            subindex = out.find("-->", index) + 3
+            out = out[:index] + out[subindex:]
     return out
+#*******************************************************************************
+def compilNex( text ):
+    f = open("html.h","w+")
+    f.write("#ifndef INC_HTML_H_\n");
+    f.write("#define INC_HTML_H_\n");
+    f.write("/*----------------------- Includes -------------------------------------*/\n");
+    f.write("#include\t\"stm32f2xx_hal.h\"\n")
+    f.write("/*------------------------ Define --------------------------------------*/\n")
+    f.write("#define\t\tHTML_LENGTH\t\t" + str( len( text ) ) + "U\t\t//" + str( len( text ) / 1024 )  +  " Kb\n")
+    f.write("static const unsigned char data__index_html[HTML_LENGTH] = {\n")
+
+    length = len( text ) / 16;
+    last = len( text ) - length * 16
+    for i in range( 0, length ):
+        f.write("\t\t")
+        for j in range( 0, 16 ):
+            f.write( hex(ord(text[i*16 + j])) + ", ")
+        f.write("\n")
+    if (last > 0):
+        f.write("\t\t")
+        for i in range( 0, last ):
+            f.write( hex(ord(text[len( text ) - last + i]))+ ", " )
+    f.write("};\n" )
+    f.write("#endif /* INC_INDEX_H_ */")
+    f.close()
+    return
 #*******************************************************************************
 def make():
     # Get paths to html, css and js files
@@ -74,6 +102,7 @@ def make():
         htmlText = removeLink(htmlText, jsFile,'js')
     # Add css section
     index = htmlText.find("</head>")
+
     htmlText = htmlText[:index] + "<style>" + htmlText[index:]
     index = index + 7
     for cssFile in cssFiles:
@@ -84,14 +113,12 @@ def make():
     htmlText = htmlText[:index] + "</style>" + htmlText[index:]
     # Add js section
     index = htmlText.find("</body>")
-
     stIndex = -1
     i = 0
     while (stIndex < 0):
         stIndex = htmlText.find("</div>",index-i,index)
         i = i + 1
     index = stIndex + 6
-
     htmlText = htmlText[:index] + "<script>" + htmlText[index:]
     index = index + 8
     for jsFile in jsFiles:
@@ -104,5 +131,8 @@ def make():
     output = open("index_make.html","w+")
     output.write(htmlText)
     output.close()
+
+    compilNex(htmlText)
 #*******************************************************************************
 make()
+#*******************************************************************************
