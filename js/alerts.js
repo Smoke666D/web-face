755a8d81b8427d9711fce40b8d1833137780e00e
Mike <photo.mickey@gmail.com>
2020-03-24 16:51:57 +0300

    Akert CSS update

--- js/alerts.js
+++ js/alerts.js
@@ -3,8 +3,10 @@ const triIco = '<i class="fas fa-exclamation-triangle"></i>'
 var alerIndex = 0;
 
 function closeAlert(id) {
-	document.getElementById('id').classList.remove("show");
-	document.getElementById('id').classList.add("fade");
+  object = document.getElementById('alert'+id);
+	object.classList.remove("show");
+	object.classList.add("fade");
+  alerIndex--;
 	return;
 }
 
@@ -23,12 +25,12 @@ function Alert(type,ico,text) {
     this.alertText += this.index;
     this.alertText += '" class="alert ';
     this.alertText += this.type;
-    this.alertText += ' alert-message d-flex p-0" role="alert"><div class="alert-icon d-flex justify-content-center align-items-center flex-grow-0 flex-shrink-0 py-3">';
+    this.alertText += ' alert-message shadow rounded d-flex p-0" role="alert"><div class="alert-icon d-flex justify-content-center align-items-center flex-grow-0 flex-shrink-0 py-3">';
     this.alertText += this.ico;
     this.alertText += '</div><div id="alert-text" class="d-flex align-items-center py-2 px-3">';
     this.alertText += this.text;
     this.alertText += '</div><a href="#" class="close d-flex ml-auto justify-content-center align-items-center px-3" onclick="closeAlert('
-    this.alertText += 'alert' + this.index;
+    this.alertText += this.index;
     this.alertText += ')"><i class="fas fa-times"></i></a></div></div>';
     box.innerHTML = box.innerHTML + this.alertText;
     this.object = document.getElementById('alert'+this.index);
@@ -41,9 +43,10 @@ function Alert(type,ico,text) {
     obj = document.getElementById('alert'+self.index);
     obj.classList.remove("show");
   	obj.classList.add("fade");
+    alerIndex--;
     setTimeout(function(){
         document.getElementById('alert'+self.index).remove();
-        alerIndex--;
+
     }, 1*1000);
   }, 3*1000);
 }
