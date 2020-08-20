9a86747f0ed52c746b14dfa04d9a22639b682918
Mike <photo.mickey@gmail.com>
2020-08-20 22:12:12 +0300

    all icons is offline + some libs

--- js/settingsAlerts.js
+++ js/settingsAlerts.js
@@ -7,7 +7,7 @@ function checkDO( targetText ) {
     }
   }
   if ( res == 0 ) {
-    let alert = new Alert("alert-warning",'<i class="fas fa-chevron-right"></i>',"Цифровой выход под данную функцию не сконфигурирован.")
+    let alert = new Alert("alert-warning",shevronRightIco,"Цифровой выход под данную функцию не сконфигурирован.")
   }
   return res;
 }
@@ -40,23 +40,23 @@ function checkSettings() {
       this.checked = false;
       document.getElementById('sinput-starterStopSpeedLevel').disabled = true;
       document.getElementById('s-slider-starterStopSpeedLevel').setAttribute('disabled', false);
-      let alert = new Alert("alert-warning",'<i class="fas fa-tachometer-alt"></i>',"Вход датчика скорости оборотов не сконфигурирован.")
+      let alert = new Alert("alert-warning",tachometerIco,"Вход датчика скорости оборотов не сконфигурирован.")
     }
   });
 
   document.getElementById('starterStopOilPressureEnb').addEventListener( 'change', function() {
     var type = document.getElementById( 'oilPressureSensorType' );
     if ( this.disabled == true ) {
-      let alert = new Alert("alert-warning",'<i class="fas fa-oil-can"></i>',"Вход датчика давления масла не сконфигурирован.")
+      let alert = new Alert("alert-warning",oilIco,"Вход датчика давления масла не сконфигурирован.")
     }
     if ( this.checked == true ) {
       if ( type.value == 0 ) {
         this.checked = false;
         document.getElementById('sinput-starterStopOilPressureLevel').disabled = true;
         document.getElementById('s-slider-starterStopOilPressureLevel').setAttribute('disabled', false);
-        let alert = new Alert("alert-warning",'<i class="fas fa-oil-can"></i>',"Вход датчика давления масла не сконфигурирован.")
+        let alert = new Alert("alert-warning",oilIco,"Вход датчика давления масла не сконфигурирован.")
       } else if ( ( type.value == 1 ) || ( type.value == 2 ) ) {
-        let alert = new Alert("alert-warning",'<i class="fas fa-oil-can"></i>',"Дискретный датчик давления масла недостаточно точен для критерия остановки стартера.")
+        let alert = new Alert("alert-warning",oilIco,"Дискретный датчик давления масла недостаточно точен для критерия остановки стартера.")
       }
     }
   });
